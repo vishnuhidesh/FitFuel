9374b5293cadb442c7b25c40396f375cb06643b5
Vishnu Hidesh <vishnuhideshnt@gmail.com>
2023-11-14 12:50:24 +0530

    Something

--- Sample Module Testing/BMI.py
+++ Sample Module Testing/BMI.py
@@ -15,11 +15,21 @@ def BMIRange(bmi):
         return "Obesity Class II"
     elif bmi >= 40.0:
         return "Obesity Class III"
+    
+def healthScore(bmi,bfp,dbp,sbp,dbs):
+    nbmi = (bmi - 18.5)/11.5
+    nbfp = (bfp - 1.0)/25.0
+    ndbp = (dbp - 60.0)/20.0
+    nsbp = (sbp - 90.0)/30.0
+    ndbs = dbs
+    score = (nbmi * 0.3) + (nbfp * 0.25) + (nsbp * 0.175) + (ndbp * 0.175) + (ndbs * 0.2)
+    return score
 
 
 
-bmi = BMI(172.0,98.0)
+bmi = BMI(172.0,81.0)
 
 
 print(round(bmi,1))
 print(BMIRange(bmi))
+print(healthScore(21,12,80.0,120.0,1))
